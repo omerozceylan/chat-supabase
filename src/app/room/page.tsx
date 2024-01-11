ef2d664cd782fe7caae868426da9f0880c0c860e
omerozceylan <omerozceylan35@gmail.com>
2024-01-11 13:58:28 +0300

    feat: room page created

---
+++ src/app/room/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { supabase } from "@/supabase/client";
+import { useEffect } from "react";
+
+async function fetchMessagesByRoomId(roomId: any) {
+  let { data: messages, error } = await supabase
+    .from("messages")
+    .select("*")
+    .eq("room_id", roomId)
+    .order("created_at", { ascending: false }); // Assuming you have a 'created_at' field
+
+  if (error) {
+    console.error("Error fetching messages", error);
+    return [];
+  }
+
+  return messages;
+}
+
+export default function Room() {
+  useEffect(() => {
+    async function someFunction() {
+      try {
+        const messages = await fetchMessagesByRoomId(
+          "cea9d03d-5812-4e36-80a3-82a5fdc7287f"
+        );
+        return messages;
+      } catch (error) {
+        console.error("Error fetching messages", error);
+        return [];
+      }
+    }
+    const data = someFunction();
+    console.log(data);
+  }, []);
+  return <div></div>;
+}
