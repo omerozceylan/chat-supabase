28442723c18dd866e681c54a08ce7201683cc7ec
omerozceylan <omerozceylan35@gmail.com>
2024-01-19 03:53:48 +0300

    feat: tailwind update

---
+++ src/components/Messages/MessageInputContainer/MessageInputContainer.tsx
@@ -0,0 +1,3 @@
+export default function MessageInputContainer() {
+  return <div></div>;
+}
--- src/components/auth/UserCard/UserCard.tsx
+++ src/components/auth/UserCard/UserCard.tsx
@@ -2,7 +2,7 @@ import { FaUserCircle } from "react-icons/fa";
 
 export default function UserCard() {
   return (
-    <div className="flex gap-3 items-center relative p-2 cursor-pointer text-sm bg-white shadow rounded-xl">
+    <div className="flex gap-3 items-center relative p-2 cursor-pointer text-sm bg-white shadow rounded-lg mb-2">
       <FaUserCircle className="w-8 h-8" />
       Omer Ozceylan
     </div>
