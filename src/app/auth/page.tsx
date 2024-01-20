39f10a60808e4201eb708b8273d3890211aa81f3
omerozceylan <omerozceylan35@gmail.com>
2024-01-20 13:54:15 +0300

    feat: add constant 404 page for auth which is reqs page tsx but not reachable

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,7 @@
 export default function Auth() {
-  return <div></div>;
+  return (
+    <div className="bg-zinc-50 flex justify-center items-center h-screen text-black">
+      404 page not found
+    </div>
+  );
 }
