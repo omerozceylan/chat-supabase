32c06030ba6a96ff7f4d411b1ad5ab81c4618d0c
omerozceylan <omerozceylan35@gmail.com>
2024-01-29 05:18:08 +0300

    feat added links to auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
+
 export default function Auth() {
   return (
     <div className="bg-zinc-50 flex justify-center  items-center h-screen text-black">
       <div className="bg-white p-4 flex flex-col gap-4 rounded-md border">
         To reach chats you have to login.
         <div className="flex justify-end gap-4">
-          <button className="">login</button>
-          <button className="bg-black text-white px-2 py-1 rounded-md">
-            register
-          </button>
+          <Link href={"/auth/login"}>
+            <button className="">login</button>
+          </Link>
+          <Link href={"/auth/register"}>
+            <button className="bg-black text-white px-2 py-1 rounded-md">
+              register
+            </button>
+          </Link>
         </div>
       </div>
     </div>
