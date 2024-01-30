0490f7007a58cdc35662b134f238fbb333a924ef
omerozceylan <omerozceylan35@gmail.com>
2024-01-31 01:18:55 +0300

    fix: auth margin

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import { FcGoogle } from "react-icons/fc";
 
 import * as z from "zod";
@@ -30,7 +31,7 @@ const formSchema = z.object({
 });
 
 export default function Login() {
-  // ...
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -41,17 +42,26 @@ export default function Login() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     const { error } = await supabase.auth.signInWithPassword(values);
-    if (error) alert(error);
+    if (error) {
+      alert(error);
+      return;
+    }
+    router.push("/rooms");
   }
+
+  async function sigInWithGoogle() {
+    const { data, error } = supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+    console.log(data, error);
+  }
+
   return (
     <div className="min-h-screen p-4 bg-zinc-50 flex justify-center items-center">
       {" "}
-      <div>
+      <div className="space-y-6 bg-white text-black p-4 w-80 rounded-lg border">
         <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-6 bg-white text-black p-4 w-80 rounded-lg border "
-          >
+          <form onSubmit={form.handleSubmit(onSubmit)} className=" ">
             <FormField
               control={form.control}
               name="email"
@@ -89,22 +99,27 @@ export default function Login() {
                 </FormItem>
               )}
             />
-            <Button className="h-9 px-3 text-sm" type="submit">
+            <Button className="h-9 px-3 text-sm mt-3" type="submit">
               Submit
             </Button>
-            <div className="flex flex-col justify-center items-center gap-3">
-              <button className="w-full flex items-center rounded-md gap-2 bg-zinc-50 hover:bg-white border p-2 justify-center">
-                Login with Google <FcGoogle className="w-5 h-5" />
-              </button>
-              <span className="font-semibold text-sm">or</span>
-              <Link className="w-full" href={"/auth/register"}>
-                <button className="w-full flex items-center rounded-md gap-2 bg-zinc-50 hover:bg-white border p-2 justify-center ">
-                  register
-                </button>
-              </Link>
-            </div>
           </form>
         </Form>
+        <div className="flex flex-col justify-center items-center gap-3">
+          <button
+            onClick={() => {
+              sigInWithGoogle();
+            }}
+            className="w-full flex items-center rounded-md gap-2 bg-zinc-50 hover:bg-white border p-2 justify-center"
+          >
+            Login with Google <FcGoogle className="w-5 h-5" />
+          </button>
+          <span className="font-semibold text-sm">or</span>
+          <Link className="w-full" href={"/auth/register"}>
+            <button className="w-full flex items-center rounded-md gap-2 bg-zinc-50 hover:bg-white border p-2 justify-center ">
+              register
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,21 +1,15 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Auth() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/auth/login");
+  }, []);
+
   return (
-    <div className="bg-zinc-50 flex justify-center  items-center h-screen text-black">
-      <div className="bg-white p-4 flex flex-col gap-4 rounded-md border">
-        To reach chats you have to login.
-        <div className="flex justify-end gap-4 items-center">
-          <Link href={"/auth/login"}>
-            <button className="">login</button>
-          </Link>
-          <Link href={"/auth/register"}>
-            <button className="bg-black text-white px-2 py-1 rounded-md">
-              register
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <div className="bg-zinc-50 flex justify-center  items-center h-screen text-black"></div>
   );
 }
