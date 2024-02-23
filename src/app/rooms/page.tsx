4124702e924e8306dda777f27bc6de7565e8bf46
omerozceylan <omerozceylan35@gmail.com>
2024-02-23 21:00:02 +0300

    feat: test

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -5,6 +5,7 @@ import ColorThemeContext from "@/Context/ColorThemeProvider";
 import { MessageContainer } from "@/components";
 import ParticipantsContainer from "@/components/Room/ParticipantsContainer/ParticipantsContainer";
 import MainContextProvider from "@/MainContext";
+import { supabase } from "@/supabase/client";
 import { Suspense } from "react";
 
 export default function Room() {
